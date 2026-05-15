0c322071beb9cfc363d6a4678184ee4ecfe53f31
ashutoshray-dev <ashukr.info1@gmail.com>
2026-05-16 00:10:25 +0530

    setup utils in the backend

--- backend/utils.py
+++ backend/utils.py
@@ -1,29 +1,12 @@
-from langgraph.graph import StateGraph, START, END
-from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, AIMessageChunk
-from langgraph.graph.message import add_messages
-from langchain_core.runnables import RunnablePassthrough, RunnableLambda
-from typing import TypedDict, Annotated, List
-from langgraph.checkpoint.sqlite import SqliteSaver
-import sqlite3
 from dotenv import load_dotenv
 load_dotenv()
 import os
 from langsmith import traceable
-from langchain_core.runnables import RunnableConfig
-from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
-from langchain_community.retrievers import BM25Retriever
-from langchain_classic.retrievers import EnsembleRetriever
-import pickle
-from pydantic import BaseModel, Field
-from langchain_google_genai import ChatGoogleGenerativeAI
-from sentence_transformers import CrossEncoder
+from .models import *
+from .checkpoint import checkpointer
 
 os.environ["LANGCHAIN_PROJECT"] = "Production-RAG-system"
 
