4e2a31c49fccb5fdec2f1038ac8926a7481856cd
ashutoshray-dev <ashukr.info1@gmail.com>
2026-05-28 23:32:37 +0530

    adjusting to new collections version of the ragas metrics

--- tests/eval_ragas.py
+++ tests/eval_ragas.py
@@ -2,7 +2,8 @@ import json
 import os
 from datasets import Dataset
 from ragas import evaluate
-from ragas.metrics import _faithfulness, _answer_relevance, _context_precision
+# from ragas.metrics import _faithfulness, _answer_relevance, _context_precision
+from ragas.metrics.collections import Faithfulness, ContextPrecision, AnswerRelevancy
 import sys
 from pathlib import Path
 # root_dir = Path(os.getcwd()).parent
@@ -11,11 +12,15 @@ from pathlib import Path
 # root_dir = current_file_path.parent.parent
 # if str(root_dir) not in sys.path:
 #     sys.path.append(root_dir)
-
 # print(root_dir)
 from backend.graph import system
+from backend.models import model
+from ragas.llms import llm_factory
+from ragas.embeddings import embedding_factory
 
-
+client = os.getenv("GOOGLE_API_KEY")
+eval_llm = llm_factory('gemini-3-flash-preview', client=client)
+eval_embeddings = embedding_factory('huggingface', model='BAAI/bge-small-en-v1.5')
 def run_rag_on_testset(test_set_path:str):
     """Feeds test questions to Langgraph and captures its full runtime state."""
     with open(test_set_path, 'r', encoding='utf-8') as f:
@@ -29,7 +34,7 @@ def run_rag_on_testset(test_set_path:str):
     print(f"Running evaluation test on {len(golden_data)} benchmarked items...")
     for item in golden_data:
         user_query = item['user_input']
-        initial_state = {"user_input": user_query}
+        initial_state = {"messages": [user_query]}
         config = {'configurable': {"thread_id":"eval_session_prod"}}
         final_state = system.invoke(initial_state, config=config)
         queries.append(user_query)
@@ -49,15 +54,17 @@ def main():
     if not os.path.exists(eval_file):
         print(f"Error: Evaluation set missing at {eval_file}")
         return
-    
+    metrics = [
+    Faithfulness(llm=eval_llm),
+    ContextPrecision(llm=eval_llm),
+    AnswerRelevancy(llm=eval_llm, embeddings=eval_embeddings) # Added embeddings here
+]
     runtime_data = run_rag_on_testset(eval_file)
     eval_dataset = Dataset.from_dict(runtime_data)
     print("Running LLM-as-Judge evalaution matrix via Ragas: ")
     result = evaluate(
         dataset=eval_dataset,
-        metrics=[
-            _faithfulness, _answer_relevance, _context_precision
-        ]
+        metrics=metrics
     )
     print("Evaluation summary------------------------------------------------")
     print(result)
